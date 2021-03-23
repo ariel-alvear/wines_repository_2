9afd8f5e9b680a83c4adf02d725dc42758d5f7af
Ariel Alvear <ariel.alvear.l@gmail.com>
2021-03-23 18:40:22 -0300

    seed.rb done, now we can continue

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Strain.destroy_all
-strains = Strain.create([{ name: 'Merlot'}, {name: 'Carmenere'}, {name: 'Cabernet Souvignon'}, {name: 'Chardonnay'}, {name: 'Malbec'}, {name: 'Pinot Noir'}, {name: 'Souvignon Blanc'}, {name: 'Garnacha'}])
-
 Wine.destroy_all
 wines = Wine.create([{ name: 'Santa Helena'}, {name: 'Casillero del Diablo'}, {name: 'Tres Medallas'}])
+
+Strain.destroy_all
+strains = Strain.create([{ name: 'Merlot'}, {name: 'Carmenere'}, {name: 'Cabernet Souvignon'}, {name: 'Chardonnay'}, {name: 'Malbec'}, {name: 'Pinot Noir'}, {name: 'Souvignon Blanc'}, {name: 'Garnacha'}])
 
 WineStrain.destroy_all
 wine_strains = WineStrain.create([{ wine_id: 1, strain_id: 1, percentage: 80}, { wine_id: 1, strain_id: 2, percentage: 20}])
 
+
+User.destroy_all
+users = User.create([{email: 'admin@admin.com', password: 'valid_password', password_confirmation: 'valid_password'}, {email: 'friend@friend.com', password: 'valid_password', password_confirmation: 'valid_password'}])
+
+Role.destroy_all
+roles = Role.create([{name: 'admin'}, {name: 'friend'}])
+
+Assignment.destroy_all
+assignments = Assignment.create([{user_id: 1, role_id: 1}, {user_id: 2, role_id: 2}])
 
